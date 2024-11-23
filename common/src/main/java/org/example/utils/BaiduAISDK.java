8b1785d0d5653fd31d9ae064ce3a4c28f6edeef2
yx-Feng <1906574057@qq.com>
2024-11-23 10:04:57 +0800

    语音SDK接入，注意修改AppID、API Key和Secret Key

---
+++ common/src/main/java/org/example/utils/BaiduAISDK.java
@@ -0,0 +1,11 @@
+package org.example.utils;
+
+import org.apiguardian.api.API;
+
+public class BaiduAISDK {
+
+    public static final String APP_ID = "123";
+    public static final String API_KEY = "123";
+    public static final String SECRET_KEY = "123";
+
+}
